d595c0774c8afd21442ea54c2826795da6cad255
Nikita Rodionov <NekoNekt@gmail.com>
2012-07-19 15:10:17 +0400

    full chain

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ usage =\
 Запросы:
   Добавить категорию:
 
-    <request type = "add_doc">
+    <request type = "add_topic">
         <topic id = "[id]">
             <title>[title]</title>
             <concepts>
@@ -33,16 +33,53 @@ usage =\
 
     <request type = "doc_num">
         <topic id = "[id]"/>
+    </request>
+
+  Добавить документ:
+
+    <request type = "add_doc">
+        <doc url = "[url]">
+            <topic id ] = "[id]"/>
+            ...
+        </docum>
     </request>"""
 
 addres = "tcp://localhost:5554"
 
+add_doc_wiki = """\
+<request type = "add_doc">
+    <doc url = "http://ru.wikipedia.org">
+        <topic id = "000.000.000"/>
+    </doc>
+</request>
+"""
 show_generic_children = """\
 <request type = "show_children">
     <topic id = "000.000.000"/>
 </request>
 """
-tests = [show_generic_children]
+show_010_children = """\
+<request type = "show_children">
+    <topic id = "010.000.000"/>
+</request>
+"""
+show_010_010_children = """\
+<request type = "show_children">
+    <topic id = "010.010.000"/>
+</request>
+"""
+show_010_010_010_children = """\
+<request type = "show_children">
+    <topic id = "010.010.010"/>
+</request>
+"""
+show_generic_docs = """\
+<request type = "show_docs">
+    <topic id = "000.000.000"/>
+</request>
+"""
+
+tests = [add_doc_wiki, show_generic_children, show_010_children, show_010_010_children, show_010_010_010_children, show_generic_docs]
 
 def client_start():
     context = zmq.Context()
@@ -52,12 +89,14 @@ def client_start():
     socket = context.socket(zmq.REQ)
     socket.connect(addres)
 
-    for test in tests:
-        print ">>> Sending request:\n ", test, "\n..."
-        socket.send(test)
+    while(True):
+        for test in tests:
+            raw_input("Нажмите Enter, чтобы отправить новый запрос:")
+            print ">>> Sending request:\n ", test, "\n..."
+            socket.send(test)
 
-        reply = socket.recv()
-        print ">>> Recived reply:\n", reply
+            reply = socket.recv()
+            print ">>> Recived reply:\n", reply
 
 
 
